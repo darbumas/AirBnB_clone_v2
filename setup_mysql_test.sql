b72166b27df20c8034cc3e5a6c8b1a89089bb6b7
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-03-03 16:37:40 -0600

    adding sql files

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- TEST
+-- write a script that cretes database, adds user and sets privs
+-- silent on failure
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
