e515a60cdc74b0fa42cc487680b625f371c163bf
darbumas <2314@holbertonschool.com>
2022-03-03 14:58:17 +0000

    updating base_model: moving 'storage.new()'

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -39,6 +38,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
