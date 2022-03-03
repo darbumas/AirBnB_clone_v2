848baf50eeb76814820e4ee23368cff24adc2757
darbumas <2314@holbertonschool.com>
2022-03-03 18:17:57 +0000

    revert changes

--- models/city.py
+++ models/city.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-<<<<<<< HEAD
-import models
-from os import environ
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 168a0ba9cb1e28b9d47a30884a73d0e53bbb58a8
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities', cascade='all, delete-orphan')
-    else:
-        state_id = ""
-        name = ""
-=======
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -32,4 +12,3 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
     places = relationship('Places', backref='cities', cascade='all')
->>>>>>> 23930bb2b4653485181bdb5643d77ce988e6e53f
--- models/user.py
+++ models/user.py
@@ -1,49 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-<<<<<<< HEAD
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-import os
-=======
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
->>>>>>> 23930bb2b4653485181bdb5643d77ce988e6e53f
-
-
-class User(BaseModel, Base):
-    """This class defines a User object"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-<<<<<<< HEAD
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship('Places', backref='user', cascade='all')
->>>>>>> 23930bb2b4653485181bdb5643d77ce988e6e53f
-=======
-=======
->>>>>>> parent of 9f2cbb7... confirm changes
-from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
->>>>>>> 168a0ba9cb1e28b9d47a30884a73d0e53bbb58a8
-<<<<<<< HEAD
->>>>>>> parent of 9f2cbb7... confirm changes
-=======
->>>>>>> parent of 9f2cbb7... confirm changes
+    places = relationship('Places', backref='user', cascade='all')
