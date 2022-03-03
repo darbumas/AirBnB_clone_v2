d1a040c4c3940e23e2314eac8115827361e26399
darbumas <2314@holbertonschool.com>
2022-03-03 18:06:10 +0000

    revert

--- models/user.py
+++ models/user.py
@@ -22,8 +22,22 @@ class User(BaseModel, Base):
 
 
 =======
+<<<<<<< HEAD
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
     places = relationship('Places', backref='user', cascade='all')
 >>>>>>> 23930bb2b4653485181bdb5643d77ce988e6e53f
+=======
+from sqlalchemy import Column, String
+
+
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+>>>>>>> 168a0ba9cb1e28b9d47a30884a73d0e53bbb58a8
+>>>>>>> parent of 9f2cbb7... confirm changes
