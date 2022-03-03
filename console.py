2a9e92ddf5e2248a33af1844a34e14f1040d81c1
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-03-03 11:29:28 -0600

    modified do create function

--- console.py
+++ console.py
@@ -116,37 +116,38 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """Creates a new instance of a class"""
-        if not args:
-            print("** class name missing **")
-            return
-        if args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not args:
+                raise SyntaxError()
+            my_list = args.split(" ")
 
-        # create object
-        args = shlex.split(args)
-        obj = eval(args[0] + "()")
-        while args:
-            arg = args.pop(0)
-            if arg in HBNBCommand.types:
-                obj.__dict__[arg] = HBNBCommand.types[arg](arg)
-            else:
-                obj.__dict__[arg] = arg
-            if '=' in arg:
-                arg = arg.partition('=')
-                obj.__dict__[arg[0]] = arg[2]
-                if val.isdigit(c_id):
-                    obj.__dict__[arg[0]] = int(arg[2])
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
                 else:
                     try:
-                        obj.__dict__[arg[0]] = float(arg[2])
-                        val = float(arg[2])
-                    except ValueError:
-                        pass
-            setattr(obj, arg, arg)
-        obj.save()
-        print(obj.id)
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
 
 
 
