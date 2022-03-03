b820d1fa64fb31f8330b1447d2ae506d054d6bba
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-03-02 18:28:36 -0600

    merge to main

--- models/state.py
+++ models/state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, relationship
+import os
 
 
 class State(BaseModel):
