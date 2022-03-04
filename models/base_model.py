5cd5f66a677d9d9ae8f2431e73ad38a769077f5b
darbumas <2314@holbertonschool.com>
2022-03-04 19:05:57 +0000

    quick fix

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 if key != '__class__':
                     setattr(self, key, value)
         else:
