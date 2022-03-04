c32b13599cdfd28bc6586cbfc45d0ad6a1833bb4
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-03-03 20:17:14 -0600

    updating to get second set of eyes

--- models/user.py
+++ models/user.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a User object"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship('Place', backref='users', cascade='all, delete-orphan')
-    
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
