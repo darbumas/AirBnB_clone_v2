757ec94294076079dc84eedc515ab4d8022e39dd
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-03-03 20:00:37 -0600

    updating for task 8

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Float
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a User object"""
     __tablename__ = 'users'
@@ -9,3 +10,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship('Place', backref='users', cascade='all, delete-orphan')
+    
