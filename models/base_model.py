9b17325cdb3846cb7def5bb04b8f378d7860ea5c
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-03-04 13:16:11 -0600

    modified init

--- models/base_model.py
+++ models/base_model.py
@@ -17,16 +17,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
-                if key != '__class__':
-                    setattr(self, key, value)
-        else:
+        if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         if not hasattr(self, 'id'):
             self.id = str(uuid.uuid4())
         if not hasattr(self, 'created_at'):
